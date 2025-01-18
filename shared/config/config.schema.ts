334c5183382ca81180b5817f6a3874eceac6659b
tikkwi <tinkokowin@ucsy.edu.mm>
2025-01-18 19:32:22 +0630

    fix lock/write conflict issues Pre create all collections on connection open and resolved issues like. 1. Unable to acquire IX lock... 2. Collection namespace 'db.collection' is already in use. :: and faster

--- shared/config/config.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Max } from 'class-validator';
-import BaseSchema from '@common/core/base/base.schema';
-import AppProp from '@common/decorator/app_prop.decorator';
-
-export default class Config extends BaseSchema {
-   @AppProp({ type: Number, default: 70 })
-   @Max(90)
-   max_price_lvl_allowance: number;
-}
